38cee223cacea14a8204adb94a6e056d6abace01
ali <ali.hassan.salman.2000@gmail.com>
2025-10-16 23:27:58 +0300

    Task 5

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+    const [newBook, setNewBook] = useState(
+        { title: '', author: '' }
+    );
+
+
+    const handleSubmit = (event) => {
+        // Prevent default event behavior
+        event.preventDefault();
+        setBooks([...books, newBook]);
+        setNewBook({ title: '', author: '' });
+    };
+    const handleInputChange = (event) => {
+
+        setNewBook({ ...newBook, [event.target.name]: event.target.value });
+    };
+
+    return (
+        <>
+            <div className="bookshelfDiv">
+                <div className="formDiv">
+                    <h3>Add a Book</h3>
+                    <form>
+                        <label htmlFor="title">Title: </label>
+                        <input 
+                            id="title" 
+                            name="title" 
+                            value={newBook.title} 
+                            onChange={handleInputChange}/>
+        
+                        <label htmlFor="author">Author: </label>
+                        <input 
+                            id="author" 
+                            name="author" 
+                            value={newBook.author} 
+                            onChange={handleInputChange} />
+
+                        <button onClick={handleSubmit}>Add Book</button>
+                    </form>
+                </div>
+
+                <div className="bookCardsDiv">
+                    {books.map((book, idx) => (
+                      <div key={idx} className="book-card">
+                        <h2>{book.title}</h2>
+                        <p>{book.author}</p>
+                      </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Bookshelf;
